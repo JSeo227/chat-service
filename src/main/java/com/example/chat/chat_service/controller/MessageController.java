4a27c4ef713c3695f5f32877560f9d939384698d
J.Seo <67002666+JSeo227@users.noreply.github.com>
2025-05-12 10:15:46 +0900

    메시지 컨트롤러 수정

--- src/main/java/com/example/chat/chat_service/controller/MessageController.java
+++ src/main/java/com/example/chat/chat_service/controller/MessageController.java
@@ -1,24 +1,34 @@
 package com.example.chat.chat_service.controller;
 
 import com.example.chat.chat_service.domain.Message;
+import com.example.chat.chat_service.domain.MessageStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 public class MessageController {
 
-    @MessageMapping("/enter")
-    @SendTo("/topic/public")
-    public String enter(Message message) throws Exception {
-        return message.getSenderName();
+    @MessageMapping("/enter")   // 클라이언트가 보내는 주소
+    @SendTo("/topic/public")    // 구독자에게 전파
+    public Message enter(Message message) {
+        if (message.getStatus() == MessageStatus.ENTER) {
+            message.setContent(message.getSenderName() + "님이 입장하였습니다.");
+        }
+        return message;
     }
 
-    @MessageMapping("/send")     // 클라이언트가 보내는 주소
-    @SendTo("/topic/public")            // 사람에게 전파
-    public String sendMessage(Message message) throws Exception {
-        return message.getContent();
+    @MessageMapping("/send")
+    @SendTo("/topic/public")
+    public Message sendMessage(Message message) {
+        if (message.getStatus() == MessageStatus.TALK) {
+            return message;
+        } else {
+            Message invalidMessage = new Message();
+            invalidMessage.setContent("잘못된 상태의 메시지입니다.");
+            return invalidMessage;
+        }
     }
 }
