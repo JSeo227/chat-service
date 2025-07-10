a42f44611be6be4242ee3730a077bec67e917282
J.Seo <67002666+JSeo227@users.noreply.github.com>
2025-07-10 10:29:04 +0900

    Update KafkaConsumer.java

--- src/main/java/com/example/chat/chat_service/global/kafka/KafkaConsumer.java
+++ src/main/java/com/example/chat/chat_service/global/kafka/KafkaConsumer.java
@@ -1,4 +1,22 @@
 package com.example.chat.chat_service.global.kafka;
 
+import com.example.chat.chat_service.domain.chat.Message;
+import com.example.chat.chat_service.repository.MessageRepository;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
 public class KafkaConsumer {
+
+    private final MessageRepository messageRepository;
+
+    public KafkaConsumer(MessageRepository messageRepository) {
+        this.messageRepository = messageRepository;
+    }
+
+    @KafkaListener(topics = "chat", groupId = "chat-group")
+    public void listen(Message message) {
+//        messageRepository.save(message);
+        System.out.println("message = " + message);
+    }
 }
