eae22b800c9b4d27bc27c29be81acccbdcb22980
J.Seo <67002666+JSeo227@users.noreply.github.com>
2025-07-11 14:06:48 +0900

    시간 추가

--- src/main/java/com/example/chat/chat_service/domain/chat/Message.java
+++ src/main/java/com/example/chat/chat_service/domain/chat/Message.java
@@ -7,6 +7,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.LocalDateTime;
+
 @Document(collection = "messages")
 @Data
 @AllArgsConstructor
@@ -21,6 +23,7 @@ public class Message {
     private String senderName; // member name
     private String content; // 메시지 내용
     private Status status; // ENTER, TALK, LEAVE
+    private LocalDateTime createdAt;
 
     public Message(MessageDto messageDto) {
         this.roomId = messageDto.getRoomId();
@@ -28,5 +31,6 @@ public class Message {
         this.senderName = messageDto.getSenderName();
         this.content = messageDto.getContent();
         this.status = messageDto.getStatus();
+        this.createdAt = LocalDateTime.now();
     }
 }
