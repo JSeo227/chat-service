109474cf5c620b04b53fcaad2deaea212c4527e1
J.Seo <67002666+JSeo227@users.noreply.github.com>
2025-07-11 09:41:16 +0900

    Update Kafka Config

--- src/main/java/com/example/chat/chat_service/global/config/kafka/KafkaConsumerConfig.java
+++ src/main/java/com/example/chat/chat_service/global/config/kafka/KafkaConsumerConfig.java
@@ -2,6 +2,7 @@ package com.example.chat.chat_service.global.config.kafka;
 
 import com.example.chat.chat_service.domain.chat.Message;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
@@ -20,7 +21,7 @@ public class KafkaConsumerConfig {
         Map<String, Object> config = new HashMap<>();
         config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
         config.put(ConsumerConfig.GROUP_ID_CONFIG, "chat-group");
-        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
         return new DefaultKafkaConsumerFactory<>(config);
     }
--- src/main/java/com/example/chat/chat_service/global/config/kafka/KafkaProducerConfig.java
+++ src/main/java/com/example/chat/chat_service/global/config/kafka/KafkaProducerConfig.java
@@ -1,13 +1,14 @@
 package com.example.chat.chat_service.global.config.kafka;
 
 import com.example.chat.chat_service.domain.chat.Message;
-import com.fasterxml.jackson.databind.JsonSerializer;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -19,7 +20,7 @@ public class KafkaProducerConfig {
     public ProducerFactory<String, Message> producerFactory() {
         Map<String, Object> config = new HashMap<>();
         config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
-        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         return new DefaultKafkaProducerFactory<>(config);
     }
